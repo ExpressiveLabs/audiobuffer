cf07a7576b07b92bcd9de919bffcd137e5f714d8
layetri <daniel@expressivelabs.net>
2025-12-03 14:15:26 +0100

    Add test for operator overloads

--- src/ops.rs
+++ src/ops.rs
@@ -211,3 +211,63 @@ impl<T: Float + Clone> std::ops::Neg for AudioBuffer<T> {
         result
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add_buffers() {
+        let mut buf1 = AudioBuffer::new_with_size(2, 3);
+        buf1.data[0] = vec![1.0, 2.0, 3.0];
+        buf1.data[1] = vec![4.0, 5.0, 6.0];
+
+        let mut buf2 = AudioBuffer::new_with_size(2, 3);
+        buf2.data[0] = vec![10.0, 20.0, 30.0];
+        buf2.data[1] = vec![40.0, 50.0, 60.0];
+
+        // Test adding two buffers
+        let result = buf1 + buf2;
+        assert_eq!(result.data[0], vec![11.0, 22.0, 33.0]);
+        assert_eq!(result.data[1], vec![44.0, 55.0, 66.0]);
+
+        // Test adding scalar
+        let result = result + 5.0;
+        assert_eq!(result.data[0], vec![16.0, 27.0, 38.0]);
+        assert_eq!(result.data[1], vec![49.0, 60.0, 71.0]);
+
+
+        // Test in-place addition
+        let mut buf3 = AudioBuffer::new_with_size(2, 3);
+        buf3.data[0] = vec![1.0, 1.0, 1.0];
+        buf3.data[1] = vec![2.0, 2.0, 2.0];
+        buf3 += buf3.clone();
+        assert_eq!(buf3.data[0], vec![2.0, 2.0, 2.0]);
+        assert_eq!(buf3.data[1], vec![4.0, 4.0, 4.0]);
+
+        // Test in-place addition with scalar
+        buf3 += 2.0;
+        assert_eq!(buf3.data[0], vec![4.0, 4.0, 4.0]);
+        assert_eq!(buf3.data[1], vec![6.0, 6.0, 6.0]);
+
+        // Test negation
+        let neg_buf = -buf3.clone();
+        assert_eq!(neg_buf.data[0], vec![-4.0, -4.0, -4.0]);
+        assert_eq!(neg_buf.data[1], vec![-6.0, -6.0, -6.0]);
+
+        // Test subtraction
+        let sub_buf = buf3.clone() - 2.0;
+        assert_eq!(sub_buf.data[0], vec![2.0, 2.0, 2.0]);
+        assert_eq!(sub_buf.data[1], vec![4.0, 4.0, 4.0]);
+
+        // Test multiplication
+        let mul_buf = buf3.clone() * 2.0;
+        assert_eq!(mul_buf.data[0], vec![8.0, 8.0, 8.0]);
+        assert_eq!(mul_buf.data[1], vec![12.0, 12.0, 12.0]);
+
+        // Test division
+        let div_buf = buf3.clone() / 2.0;
+        assert_eq!(div_buf.data[0], vec![2.0, 2.0, 2.0]);
+        assert_eq!(div_buf.data[1], vec![3.0, 3.0, 3.0]);
+    }
+}
